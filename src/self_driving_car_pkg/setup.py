55cc58e534c4f7020500f579f80b5711e3e6a01e
Anass Bouchtaoui <bouchtaoui.anass@outlook.com>
2022-08-22 12:21:30 +0100

    Fix setup.py

--- src/self_driving_car_pkg/setup.py
+++ src/self_driving_car_pkg/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os 
+from glob import glob
 
 package_name = 'self_driving_car_pkg'
 
@@ -10,6 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name,'launch'), glob('launch/*')),
+        (os.path.join('share', package_name,'worlds'), glob('worlds/*')),
+        (os.path.join('lib', package_name), glob('scripts/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
