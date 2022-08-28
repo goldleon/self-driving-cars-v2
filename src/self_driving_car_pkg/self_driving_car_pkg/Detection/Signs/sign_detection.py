78da224c6ad55b67c239ac1f035846b134d07ae2
Anass Bouchtaoui <bouchtaoui.anass@outlook.com>
2022-08-28 16:50:01 +0100

    Refactoring Code

--- src/self_driving_car_pkg/self_driving_car_pkg/Detection/Signs/sign_detection.py
+++ src/self_driving_car_pkg/self_driving_car_pkg/Detection/Signs/sign_detection.py
@@ -1,35 +1,34 @@
 import cv2
 import numpy as np
 import os
-from tensorflow.keras.models import load_model
-import tensorflow as tf
 
-from ..tracker import Tracker
-from tensorflow.keras.utils import to_categorical
-from ...config import config
 # Training_Variables
 save_dataset = True
 iter = 0
 saved_no = 0
 # Classification Imports, Variables N Function (5a)
+from tensorflow.keras.models import load_model
+import tensorflow as tf
 
 model_loaded = False
 model = 0
 sign_classes = ["speed_sign_30", "speed_sign_60", "speed_sign_90", "stop", "left_turn",
                 "No_Sign"]  # Trained CNN Classes
+from ..tracker import Tracker
+from tensorflow.keras.utils import to_categorical
 
 sign_tracker = Tracker()
 
 
-def image_for_keras(image):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Image everywhere is in rgb but Opencv does it in BGR convert Back
+def image_forKeras(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Image everywher is in rgb but Opencv does it in BGR convert Back
     image = cv2.resize(image, (30, 30))  # Resize to model size requirement
     image = np.expand_dims(image, axis=0)  # Dimension of model is [Batch_size, input_row,inp_col , inp_chan]
     return image
 
 
 def sign_det_n_track(gray, frame, frame_draw):
-    if sign_tracker.mode == config.DETECTION:
+    if sign_tracker.mode == "Detection":
 
         circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 100, param1=250, param2=30, minRadius=20, maxRadius=100)
 
@@ -42,35 +41,35 @@ def sign_det_n_track(gray, frame, frame_draw):
                 radius = i[2] + 5
                 # 4c. Extracting Roi from localized circle
                 try:
-                    start_point = (center[0] - radius, center[1] - radius)
-                    end_point = (center[0] + radius, center[1] + radius)
+                    startP = (center[0] - radius, center[1] - radius)
+                    endP = (center[0] + radius, center[1] + radius)
 
-                    localized_sign = frame[start_point[1]:end_point[1], start_point[0]:end_point[0]]
+                    localized_sign = frame[startP[1]:endP[1], startP[0]:endP[0]]
                     # 4d. Indicating localized potential sign on frame and also displaying seperatly
                     cv2.circle(frame_draw, (i[0], i[1]), i[2], (0, 165, 255), 1)  # draw the outer circle
                     cv2.circle(frame_draw, (i[0], i[1]), 2, (0, 0, 255), 3)  # draw the center of the circle
                     cv2.imshow("ROI", localized_sign)
-                    sign = sign_classes[np.argmax(model(image_for_keras(localized_sign)))]
+                    sign = sign_classes[np.argmax(model(image_forKeras(localized_sign)))]
                     # 5d. Check if Classified Region is a Sign
-                    if sign != 'No_Sign':
+                    if sign != "No_Sign":
                         # 4i. Display Class
-                        cv2.putText(frame_draw, sign, (end_point[0] - 80, start_point[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1,
+                        cv2.putText(frame_draw, sign, (endP[0] - 80, startP[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1,
                                     (0, 0, 255), 1)
                         # 4d. Display confirmed sign in green circle
                         cv2.circle(frame_draw, (i[0], i[1]), i[2], (0, 255, 0), 2)  # draw the outer circle
 
-                        match_found, match_idx = sign_tracker.match_current_center_to_known(center)
-                        # 4f. If match found , Increment ... known centers confidence 
+                        match_found, match_idx = sign_tracker.MatchCurrCenter_ToKnown(center)
+                        # 4f. If match found , Increment ... known centers confidence
                         if match_found:
                             sign_tracker.known_centers_confidence[match_idx] += 1
                             sign_tracker.known_centers_classes_confidence[match_idx][sign_classes.index(sign)] += 1
                             # 4g. Check if same sign detected 3 times , If yes initialize OF Tracker
-                            if sign_tracker.known_centers_confidence[match_idx] > 4:
+                            if (sign_tracker.known_centers_confidence[match_idx] > 4):
                                 max_value = max(sign_tracker.known_centers_classes_confidence[match_idx])
                                 max_index = sign_tracker.known_centers_classes_confidence[match_idx].index(max_value)
                                 print("This sign we are about to track is most likely ( " + str(max_value),
                                       " ) a " + sign_classes[max_index])
-                                sign_tracker.init_tracker(sign_classes[max_index], gray, frame_draw, start_point, end_point)
+                                sign_tracker.init_tracker(sign_classes[max_index], gray, frame_draw, startP, endP)
                         # 4h. Else if Sign detected First time ... Update signs location and its detected count
                         else:
                             sign_tracker.known_centers.append(center)
@@ -99,7 +98,7 @@ def sign_det_n_track(gray, frame, frame_draw):
                         if (iter % 5) == 0:
                             saved_no = saved_no + 1
                             img_dir = os.path.abspath(
-                                "self_driving_car_pkg/self_driving_car_pkg/data/live_dataset") + class_id
+                                "src/self_driving_car_pkg/self_driving_car_pkg/data/live_dataset") + class_id
                             img_name = img_dir + "/" + str(saved_no) + ".png"
                             if not os.path.exists(img_dir):
                                 os.makedirs(img_dir)
