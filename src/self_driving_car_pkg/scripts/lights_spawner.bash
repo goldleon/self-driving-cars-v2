844aa2b6110cb2013eaf69a4e9144ba2be15ded3
Anass Bouchtaoui <bouchtaoui.anass@outlook.com>
2022-08-29 21:55:43 +0100

    Update light spawner

--- src/self_driving_car_pkg/scripts/lights_spawner.bash
+++ src/self_driving_car_pkg/scripts/lights_spawner.bash
@@ -5,7 +5,7 @@ red_light_sdf=$HOME/.gazebo/models/red_light/model.sdf
 yellow_light_sdf=$HOME/.gazebo/models/yellow_light/model.sdf
 
 ros2 run self_driving_car_pkg spawner_node "$green_light_sdf" green_Light
-sleep 0.5s
+sleep 0.2s
 ros2 run self_driving_car_pkg spawner_node "$yellow_light_sdf" yellow_light
 sleep 0.2s
 ros2 run self_driving_car_pkg spawner_node "$red_light_sdf" red_light
